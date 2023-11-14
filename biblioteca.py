c836ae2dfcf0dacaa16c2f028fe8ed13277cf3f7
ValenDiFu <valendifu15@gmail.com>
2023-11-14 18:18:51 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,9 +1,34 @@
 from libro import Libro
+from socio import Socio
 
 class Biblioteca:
     def __init__(self):
         self._libros = ()
+        self._socios = ()
+        #No se si que los prestamos tengan socios o que los socios tengan prestamos
+        #self._prestamos = ()
         
     def aggLibro(self, libro: Libro):
         self._libros.append(libro)
+
+    def buscarLibro(self, titulo):
+        pass
     
+    def buscarSocio(self, numSocio):
+        pass
+        
+    def aggSocio(self, socio: Socio):
+        self._socios.append(socio)
+    
+    def librosCadEstado(self):
+        pass
+    
+    def precioLibrosExtraviados(self):
+        pass
+    
+    def solicitantesDeLibro(self):
+        pass
+        #Supongo que se refieren a los socios que se llevaron un mismo libro
+        
+    def listPrestamosDemorados(self):
+        pass
