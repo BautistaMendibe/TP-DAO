c9ca5083c8b85c27b1ae539092f738e7fb33a8e2
bauti <bautistamendibe10@gmail.com>
2023-11-14 11:45:20 -0300

    Se agregó el menú de la barra

--- interfaz.py
+++ interfaz.py
@@ -1,8 +1,8 @@
 import tkinter
 from tkinter import *
 
-def x():
-        print(f"Realizando acción:")
+def mostrar_mensaje(accion):
+    print(f"Realizando acción: {accion}")
 
 def inicio():
     ventana = Tk()
@@ -19,14 +19,13 @@ def inicio():
     barra_menu.add_cascade(label="Registración de préstamos y devoluciones", menu=menu_prestamos_devolucion)
 
     # Opciones del menú de socios
-    menu_socios.add_command(label="Registrar socio", command=x)
-    menu_socios.add_command(label="Eliminar socio", command=x)
-    menu_socios.add_command(label="Consultar socio", command=x)
+    menu_socios.add_command(label="Registrar socio", command=lambda: mostrar_mensaje("Registrar socio"))
+    menu_socios.add_command(label="Eliminar socio", command=lambda: mostrar_mensaje("Eliminar socio"))
+    menu_socios.add_command(label="Consultar socio", command=lambda: mostrar_mensaje("Consultar socio"))
     # Opciones del menú de libros
-    menu_libros.add_command(label="Registrar libro", command=x)
-    menu_libros.add_command(label="Eliminar libro", command=x)
-    menu_libros.add_command(label="Consultar libro", command=x)
+    menu_libros.add_command(label="Registrar libro", command=lambda: mostrar_mensaje("Registrar libro"))
+    menu_libros.add_command(label="Eliminar libro", command=lambda: mostrar_mensaje("Eliminar libro"))
+    menu_libros.add_command(label="Consultar libro", command=lambda: mostrar_mensaje("Consultar libro"))
 
-    
-
+    ventana.config(menu=barra_menu)
     ventana.mainloop()
