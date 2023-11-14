4d2374c522b439bda696d66961a970a16699b706
ValenDiFu <valendifu15@gmail.com>
2023-11-14 17:54:26 -0300

    Update socio.py

--- socio.py
+++ socio.py
@@ -1,7 +1,10 @@
+from prestamo import Prestamo
+
 class Socio:
     def __init__(self, nombre, numeroSocio):
         self._nombre = nombre
         self._numeroSocio = numeroSocio
+        self._prestamosDeLibro = ()
         
     @property
     def numeroSocio(self):
@@ -11,5 +14,11 @@ class Socio:
     def nombre(self):
         return self._nombre
     
-    #def agregarPrestamo(self, prestamo):
+    def agregarPrestamo(self, prestamo: Prestamo):
+        pass
         #Aca debería entrar a la bd y consultar por todos los prestamos asociados a este número de socio, ver si son 3 o si alguno esta con demora y no darselo
+        #En realidad deberia registrar los prestamos en una lista y eso deberia estar guardado en la base de datos
+        
+    def registrarDevolucion(self, prestamo: Prestamo):
+        pass
+        #Aca deberia sacar el prestamo del socio, eliminarlo
