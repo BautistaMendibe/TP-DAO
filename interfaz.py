6e6b1594e45d9e376df2eda46e87680d77d2b58a
bauti <bautistamendibe10@gmail.com>
2023-11-14 11:44:17 -0300

    Se agregó el menú de la barra

--- interfaz.py
+++ interfaz.py
@@ -1,9 +1,32 @@
 import tkinter
 from tkinter import *
 
+def x():
+        print(f"Realizando acción:")
+
 def inicio():
     ventana = Tk()
-    ventana.title("Gestión de biblioteca")
+    ventana.title("Biblioteca UTN-FRC")
     ventana.geometry("800x500")
+
+    barra_menu = Menu(ventana)
+
+    menu_socios = Menu(barra_menu)
+    barra_menu.add_cascade(label="Administración de socios", menu=menu_socios)
+    menu_libros = Menu(barra_menu)
+    barra_menu.add_cascade(label="Administración de libros", menu=menu_libros)
+    menu_prestamos_devolucion = Menu(barra_menu)
+    barra_menu.add_cascade(label="Registración de préstamos y devoluciones", menu=menu_prestamos_devolucion)
+
+    # Opciones del menú de socios
+    menu_socios.add_command(label="Registrar socio", command=x)
+    menu_socios.add_command(label="Eliminar socio", command=x)
+    menu_socios.add_command(label="Consultar socio", command=x)
+    # Opciones del menú de libros
+    menu_libros.add_command(label="Registrar libro", command=x)
+    menu_libros.add_command(label="Eliminar libro", command=x)
+    menu_libros.add_command(label="Consultar libro", command=x)
+
+    
 
     ventana.mainloop()
