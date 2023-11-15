9f0a6184e1628b7e75ab60c98cd3195b8e6ebe58
ValenDiFu <valendifu15@gmail.com>
2023-11-15 10:57:40 -0300

    Update libro.py

--- libro.py
+++ libro.py
@@ -5,10 +5,6 @@ class Libro:
         self._precioReposicion = precioReposicion
         self._estado = estado
        
-    def __str__(self) -> str:
-        return f"Codigo: {self._codigo} Titulo: {self._titulo} Precio de Reposicion: {self._precioReposicion} 
-    Estado: {self._estado}"
-     
     @property
     def titulo(self):
         return self._titulo
