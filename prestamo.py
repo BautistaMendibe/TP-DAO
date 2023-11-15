38a6d04d455e078f22ae085be0795bc1d09c8b98
ValenDiFu <valendifu15@gmail.com>
2023-11-14 22:20:51 -0300

    Update prestamo.py

--- prestamo.py
+++ prestamo.py
@@ -1,14 +1,14 @@
 from libro import Libro
-from socio import Socio
 from datetime import datetime, timedelta
 
-def Prestamo(Socio, Libro):
-    def __init__(self, idPrestamo, fechaPrestamo, diasDevolucion, diasRetraso, devuelto):
+class Prestamo:
+    def __init__(self, idPrestamo, fechaPrestamo, diasDevolucion, diasRetraso, devuelto, libro: Libro):
         self._idPrestamo = idPrestamo
         self._fechaPrestamo = fechaPrestamo
         self._diasDevolucion = diasDevolucion
         self._diasRetraso = diasRetraso
         self._devuelto = devuelto
+        self._libro = libro
     
     @property
     def fechaPrestamo(self):
@@ -21,11 +21,11 @@ def Prestamo(Socio, Libro):
     @property
     def diasRetraso(self):
         # Si la fecha de prestamo más los días que tiene para devolver es menor a la fecha actual no hay retraso
-        if fechaPrestamo.date + timedelta(days=diasDevolucion) <= datetime.now().date:
+        if self.fechaPrestamo.date + timedelta(days=self.diasDevolucion) <= datetime.now().date:
             return 0
         else:
             # El retraso se calcula como la fecha de hoy menos la fecha del prestamo más los días que nos dieron para devolverlo.
-            retraso = datetime.now().date - (fechaPrestamo.date + timedelta(days=diasDevolucion))
+            retraso = datetime.now().date - (self.fechaPrestamo.date + timedelta(days=self.diasDevolucion))
             return retraso.days
     
     @property
